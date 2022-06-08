8ec6d0ccffaf80afa268d5e64cd2df31a3cf3add
benwest03 <77653862+benwest03@users.noreply.github.com>
2022-06-08 10:20:38 +1000

    Initial Commit

---
+++ src/App.jsx
@@ -0,0 +1,30 @@
+import React from 'react'
+import About from './components/about/About'
+import Contact from './components/contact/Contact'
+import Experience from './components/experience/Experience'
+import Footer from './components/footer/Footer'
+import Header from './components/header/Header'
+import Navbar from './components/navbar/Navbar'
+import Portfolio from './components/portfolio/Portfolio'
+import Services from './components/services/Services'
+import Testimonials from './components/testimonials/Testimonials'
+import './index.css'
+
+
+const App = () => {
+  return (
+    <>
+        <Header />
+        <Navbar />
+        <About />
+        <Contact />
+        <Experience />
+        <Portfolio />
+        <Services />
+        <Testimonials />
+        <Footer />
+    </>
+  )
+}
+
+export default App
---
+++ src/components/testimonials/Testimonials.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import './testimonials.css'
+
+const Testimonials = () => {
+  return (
+    <div>Testimonials Poop</div>
+  )
+}
+
+export default Testimonials
