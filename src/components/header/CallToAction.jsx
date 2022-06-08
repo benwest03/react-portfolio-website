f72a444fbc4a90a220252f46defc13dcc72d55bf
benwest03 <77653862+benwest03@users.noreply.github.com>
2022-06-08 16:21:13 +1000

    Images Added

--- src/components/header/CallToAction.jsx
+++ src/components/header/CallToAction.jsx
@@ -4,8 +4,8 @@ import resume from '../../assets/resume.pdf'
 const CallToAction = () => {
   return (
     <div className='cta'>
-        <a href={resume} download> Download Resume</a>
-        <a href="#contact"> Let's Talk</a>
+        <a href={resume} download className='btn-nofill'> Download Resume</a>
+        <a href="#contact" className='btn'> Let's Talk</a>
     </div>
   )
 }
--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,17 +1,27 @@
 import React from 'react'
 import './header.css'
-import calltoaction from './CallToAction'
 import CallToAction from './CallToAction'
+import {ME} from '../../assets/personalPhoto.png'
 
 const Header = () => {
   return (
-    <header>
-      <div className="container header__container">
-        <h1>Hi my name is Ben Westphal</h1>
-        <h5 className="text-light">Computer Science Student</h5>
-        <CallToAction />
+    <section id = 'header'>
+      <div className="header__container"> 
+        
+        <div className="header__text__container">
+          <h4>Hi my name is</h4>
+          <h1>Ben Westphal</h1>
+          <h5 className="text-light">Computer Science Student</h5>
+          <div className="call-to-action">
+            <CallToAction />
+          </div>
+        </div>
+
+        <div className="image__container">
+          <img src = {ME} alt = "Personal Photo" />
+        </div>
       </div>
-    </header>
+    </section>
   )
 }
 
